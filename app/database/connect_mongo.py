3eb6847bbb5aad4cd36cd0fbbee2c41440f07617
FRAMAR <franco.edson.18014@gmail.com>
2025-04-19 20:04:18 -0500

    add: lógica para la conexión con mongo bd

---
+++ app/database/connect_mongo.py
@@ -0,0 +1,19 @@
+# database/conexion_mongo.py
+import os
+
+from pymongo import MongoClient
+
+
+class ConnectMongo:
+    def __init__(self):
+        # Usa variables de entorno para seguridad
+        self.uri = os.getenv("MONGO_URI")
+        self.db_name = os.getenv("MONGO_DB_NAME")
+        self.client = MongoClient(self.uri)
+        self.db = self.client[self.db_name]
+
+    def get_collection(self, name_collection):
+        return self.db[name_collection]
+
+    def close_connection(self):
+        self.client.close()
