cd46774778d78348eae22fcbf8a4755c0b5d3d48
FRAMAR <franco.edson.18014@gmail.com>
2025-04-27 00:23:37 -0500

    add: custom exception for email invalid

--- app/exceptions/auth_exceptions.py
+++ app/exceptions/auth_exceptions.py
@@ -10,6 +10,13 @@ class UserNotFoundError(AuthenticationError):
         super().__init__(message)
 
 
+class UserInactiveError(AuthenticationError):
+    """Se lanza cuando el usuario no está activo."""
+
+    def __init__(self, message="Usuario no activo."):
+        super().__init__(message)
+
+
 class IncorrectPasswordError(AuthenticationError):
     """Se lanza cuando la contraseña es incorrecta."""
 
@@ -21,4 +28,11 @@ class UserAlreadyExistsError(AuthenticationError):
     """Se lanza cuando se intenta crear un usuario que ya existe."""
 
     def __init__(self, message="El nombre de usuario ya está en uso."):
+        super().__init__(message)
+
+
+class InvalidEmailError(AuthenticationError):
+    """Se lanza cuando se intenta crear un usuario con un correo inválido."""
+
+    def __init__(self, message="El correo no es válido."):
         super().__init__(message)
